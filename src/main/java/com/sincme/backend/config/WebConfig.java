1de86a0952b8b441d9b037622e8cfdd3646316b1
Michele <micheledatii@gmail.com>
2025-06-17 10:17:29 +0800

    Tambahkan web config

---
+++ src/main/java/com/sincme/backend/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.sincme.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // boleh akses semua endpoint
+                        .allowedOrigins("http://127.0.0.1:5502") // alamat frontend kamu
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
